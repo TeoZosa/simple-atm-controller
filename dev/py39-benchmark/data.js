1205418ff24034820d57c048fbf210d49d777a04
github-action-benchmark <github@users.noreply.github.com>
2021-05-03 04:31:17 +0000

    add Benchmark (pytest) benchmark result for 9adcd07898ce882f6f247d7ac385fd8b29e692df

---
+++ dev/py39-benchmark/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1620016277014,
+  "repoUrl": "https://github.com/TeoZosa/simple-atm-controller",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9adcd07898ce882f6f247d7ac385fd8b29e692df",
+          "message": "Merge pull request #5 from TeoZosa/dependabot/github_actions/JamesIves/github-pages-deploy-action-4.1.1\n\n⬆️ Bump JamesIves/github-pages-deploy-action from 3.7.1 to 4.1.1",
+          "timestamp": "2021-05-02T21:26:47-07:00",
+          "tree_id": "0878bd1faeb83d201962b9b1de2babb593fd87b2",
+          "url": "https://github.com/TeoZosa/simple-atm-controller/commit/9adcd07898ce882f6f247d7ac385fd8b29e692df"
+        },
+        "date": 1620016274479,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 924.5542146938471,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002976173343170929",
+            "extra": "mean: 1.081602337761378 msec\nrounds: 527"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1327.8935132925187,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002176073235737852",
+            "extra": "mean: 753.0724338885389 usec\nrounds: 1021"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 9086.11352934771,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005415750834825064",
+            "extra": "mean: 110.05805692060177 usec\nrounds: 1669"
+          }
+        ]
+      }
+    ]
+  }
+}
