4f377a679544c23c1c6e95cab28d6089d404d8a5
github-action-benchmark <github@users.noreply.github.com>
2021-05-03 00:11:38 +0000

    add Benchmark (pytest) benchmark result for f4c50fd2db82d02d28c88734b15ce60ef9bad7ac

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620000588257,
+  "lastUpdate": 1620000698970,
   "repoUrl": "https://github.com/TeoZosa/simple-atm-controller",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00007759841870515297",
             "extra": "mean: 92.03767476567259 usec\nrounds: 1811"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "distinct": true,
+          "id": "f4c50fd2db82d02d28c88734b15ce60ef9bad7ac",
+          "message": ":sparkles: Add template boilerplate",
+          "timestamp": "2021-05-02T17:06:43-07:00",
+          "tree_id": "c35a9486441fc3ad1f3837667fec648dea652cfa",
+          "url": "https://github.com/TeoZosa/simple-atm-controller/commit/f4c50fd2db82d02d28c88734b15ce60ef9bad7ac"
+        },
+        "date": 1620000697374,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1020.399676627146,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00011647392480511439",
+            "extra": "mean: 980.008150635077 usec\nrounds: 551"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1414.545261948795,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006315777368957509",
+            "extra": "mean: 706.9409702891493 usec\nrounds: 1279"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8878.761984863911,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000045436906517210076",
+            "extra": "mean: 112.62831481514566 usec\nrounds: 1998"
           }
         ]
       }
